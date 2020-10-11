b6db8fb850a1ace5e06618327a2c71619550a8dc
Nicola Da Rold <nicola.darold@gmail.com>
2020-10-11 17:22:31 +0200

    empty lazy-loading login module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    // { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
+    },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
